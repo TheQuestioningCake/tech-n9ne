f5356dd48b3649ccf0b24d08c812230f1be5c518
TheQuestioningCake <perrywayne39@gmail.com>
2024-06-05 14:09:05 -0400

    added more logic to user-routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -13,9 +13,25 @@ router.get('/', (req, res) => {
 
 router.get('/', (req, res) => {
     User.findOne({
+        attributes: {
+            exlude: ['password']
+        },
         where: {
             id: req.params.id
+        },
+        include: [{
+            model: Post,
+            attributes: ['id', 'title', 'post_content', 'created_at']
+        },
+        {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'created_at'],
+            include:{
+                model: Post,
+                attributes: ['title']
+            }
         }
+    ]
     })
     .then(dbUserData => {
         if(!dbUserData) {
@@ -24,6 +40,58 @@ router.get('/', (req, res) => {
     })
     .catch(err => {
         console.log(err);
-        res.status(500).json(err)
+        res.status(500).json(err);
+    });
+});
+
+router.post('/', (req, res) => {
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
     })
-})
+    .then((dbUserData) => {
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json(dbUserData);
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+});
+
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'Email not associated with any user'});
+            return;
+        }
+    const validPassword = dbUserData.checkPassword(req.body.password)
+
+    if (!validPassword) {
+        res.status(400).json({ message: 'Invalid Password!!!'})
+    }
+
+    req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+
+        res.json({ user: dbUserData, message: " You're logged in!!!"})
+    })
+    })
+    .catch((err) => {
+        console.log(err)
+        res.status(500).json(err)
+    });
+});
