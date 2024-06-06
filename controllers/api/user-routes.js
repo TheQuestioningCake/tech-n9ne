0151bcdd725ca3b433c6d5644c6b1f3795f4f053
TheQuestioningCake <perrywayne39@gmail.com>
2024-06-06 11:31:14 -0400

    finished logic for user-routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -95,3 +95,51 @@ router.post('/login', (req, res) => {
         res.status(500).json(err)
     });
 });
+
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
+router.put('/:id', (req, res) => {
+    User.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    })
+    .then((dbUserData) => {
+        if (!dbUserData) {
+            res.status(404).json({ message:'User does not exist'});
+            return;
+        }
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err)
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then((dbUserData) => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'User does not exist'});
+            return;
+        }
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err)
+    });
+});
+
+module.exports = router;
